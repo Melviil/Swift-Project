89990e66b68faf334f767939eef7c9cd12b158fd
Melvil Donnart <melvildonnart3@gmail.com>
2018-02-26 18:27:09 +0100

    activités

---
+++ Swift Project/ListTypeActiviteController.swift
@@ -0,0 +1,57 @@
+//
+//  ListTypeActiviteController.swift
+//  Swift Project
+//
+//  Created by Melvil on 26/02/2018.
+//  Copyright © 2018 Melvil. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ListTypeActiviteController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var typesactivite : [TypeActivite] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        let request  : NSFetchRequest<TypeActivite> = TypeActivite.fetchRequest()
+        
+        do{
+            try self.typesactivite = context.fetch(request)
+        }
+        catch{
+            return
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.typesactivite.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.typesactivite[row].libelleTypeActivite
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return self.typesactivite.count
+    }
+    
+}
