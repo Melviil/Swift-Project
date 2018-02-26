80521911f60433257072e8a605b30f1f363e73f0
Melvil Donnart <melvildonnart3@gmail.com>
2018-02-26 14:03:02 +0100

    Actualité

--- Swift Project/ViewController.swift
+++ Swift Project/ViewController.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
 
     @IBOutlet weak var ContactButton: UIButton!
     
     
+    @IBAction func ButtonAjouterTypeActivite(_ sender: Any) {
+        let alert = UIAlertController( title : " Nouveau type d'activité", message : "Ajouter un type d'activité", preferredStyle : .alert)
+        
+        let saveAction = UIAlertAction ( title :"Ajouter", style : .default )
+        {
+            [unowned self ] action in
+            guard let textField = alert.textFields?.first,
+                let typeActivityToSave = textField.text else {
+                    return
+            }
+            self.saveNewTypeActivity(withName : typeActivityToSave)
+        }
+    }
     
+    func saveNewTypeActivity( withName typeActivityToSave : String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+          //  self.alertError(errorMsg: " Could not save type ", userInfo : "Unknown reason" )
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        let type = TypeActivite(context : context)
+        type.libelleTypeActivite = typeActivityToSave
+        do{
+            try context.save()
+        }
+        catch let error as NSError{
+            //  self.alertError(errorMsg: " Could not save type ", userInfo : "Unknown reason" )
+            return
+        }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +57,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+   
 }
 
