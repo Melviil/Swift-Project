b7a116bbb30ab3524d299d6f55d797f80dea3086
Melvil Donnart <melvildonnart3@gmail.com>
2018-03-04 08:24:06 +0100

    lets start symptoms

---
+++ Swift Project/ChoixTypeSuiviController.swift
@@ -0,0 +1,55 @@
+
+//
+//  ChoixTypeSuiviController.swift
+//  Swift Project
+//
+//  Created by Melvil on 03/03/2018.
+//  Copyright © 2018 Melvil. All rights reserved.
+//
+
+import UIKit
+
+class ChoixTypeSuiviController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+   
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.dataSource = self
+        picker.delegate = self
+        pickerData = ["ON", "OFF", "DISKYNESIE", "OUBLIE"]
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swift Project/HeureAlarmActiviteController.swift
+++ Swift Project/HeureAlarmActiviteController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class HeureAlarmActiviteController: UIViewController {
+class HeureAlarmActiviteController: UIViewController, UITableViewDataSource {
+    
+   
     
     @IBOutlet weak var TableViewHours: UITableView!
+    var hours: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hours.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.hours[indexPath.row]
+        return cell
+    }
+
+
+    
+   
     @IBAction func unwindToHourListAfterSavingHour(segue: UIStoryboardSegue){
         print("I'm back")
         let newHeure = segue.source as! AjoutHeureModallyControllerViewController
         print(newHeure.heureChoisie)
+        hours.append(newHeure.heureChoisie)
+        TableViewHours.reloadData()
         
     }
     
