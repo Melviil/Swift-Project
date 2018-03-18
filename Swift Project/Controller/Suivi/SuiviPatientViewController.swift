e7a46539b5225390e4899b64bc07cb747306ca96
Melvil Donnart <melvildonnart3@gmail.com>
2018-03-18 16:50:53 +0100

    add swith jour

--- Swift Project/Controller/Suivi/SuiviPatientViewController.swift
+++ Swift Project/Controller/Suivi/SuiviPatientViewController.swift
@@ -8,42 +8,61 @@
 
 import UIKit
 
-class SuiviPatientViewController: UIViewController {
+class SuiviPatientViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
 
+    var jours = ["Jour 1","Jour 2","Jour 3","Jour 4","Jour 5"]
+    var dateSelected : Int = 0
+    
+    @IBOutlet weak var tableJour: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableJour.dataSource = self
+        tableJour.delegate = self
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
 
+    @IBAction func indexChanged(_ sender: Any) {
+        
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            dateSelected = 1;
+        case 1:
+            dateSelected = 2;
+        case 2:
+            dateSelected = 3;
+        case 3:
+            dateSelected = 4;
+        case 4:
+            dateSelected = 5;
+        default:
+            break
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    
-    @IBOutlet weak var tableJour: UITableView!
-    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jours.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = UITableViewCell()
-//        cell.textLabel?.text = String(describing: self.activites[indexPath.row])
-//        return cell
-        var array = ["Jour 1","Jour 2","Jour 3","Jour 4","Jour 5"]
         
-        let cell: UITableViewCell! = self.tableJour
-            .dequeueReusableCell(withIdentifier: "cell") as UITableViewCell!
         
-        cell.textLabel!.text = array[indexPath.row]
-       
+
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(describing: self.jours[indexPath.row])
         return cell
     }
     
